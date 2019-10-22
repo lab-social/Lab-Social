51c0904c7038e91287d417e43b6ef5b9fee8f14c
Isaac J Moore <oscillating.spheres@gmail.com>
2019-10-22 15:11:13 -0500

    small edit

--- social/config/config.php
+++ social/config/config.php
@@ -1,14 +1,10 @@
 <?php
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
-	define('DB_PASS', '123456');
+	define('DB_PASS', '1234');
 	define('DB_NAME', 'social_lab'); 
 	define('ROOT_URL', 'http://localhost/social/');
 	
-	// define('DB_HOST', 'mysql.lab-social.xyz');
-	// define('DB_USER', 'deep_diver');
-	// define('DB_PASS', 'HardCore69!');
-	// define('DB_NAME', 'lab_testing'); 
-	// define('ROOT_URL', 'www.social-lab.xyx/');
+
 
 ?>
